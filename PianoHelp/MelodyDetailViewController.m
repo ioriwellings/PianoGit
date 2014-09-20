34ea6a3164fafb82882b05cb20ee50e209069df4
yizhq <yizhqi@gmail.com>
2014-09-20 13:56:26 +0800

    ffff

--- PianoHelp/MelodyDetailViewController.m
+++ PianoHelp/MelodyDetailViewController.m
@@ -72,6 +72,7 @@
         [self.sliderXiaoJie setEnabled:false];
         [self.btnXiaoJieTiaoZhuan setEnabled:false];
         [self.btnSplitSection setEnabled:false];
+        [self.btnRePlay setEnabled:false];
     }else if(self.iPlayMode == 2){//跟弹模式
         [self.btnHint setEnabled:false];
         [self.sliderSpeed setEnabled:true];
@@ -79,6 +80,7 @@
         [self.sliderXiaoJie setEnabled:true];
         [self.btnXiaoJieTiaoZhuan setEnabled:true];
         [self.btnSplitSection setEnabled:true];
+        [self.btnRePlay setEnabled:true];
     }
     
     //    ScroeViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ScroeViewController"];
@@ -703,8 +705,10 @@
         
         if (_iPlayMode == 2) {
             [self.btnHint setEnabled:false];
+            [self.btnRePlay setEnabled:true];
         }else if (_iPlayMode == 1){
             [self.btnHint setEnabled:true];
+            [self.btnRePlay setEnabled:false];
         }
     }else if(state == 2){//end playing
         [self.btnTryListen setEnabled:true];
@@ -712,10 +716,12 @@
             [self.btnHint setEnabled:false];
             [self.sliderXiaoJie setEnabled:true];
             [self.sliderSpeed setEnabled:false];
+            [self.btnRePlay setEnabled:true];
         }else if (_iPlayMode == 1){
             [self.btnHint setEnabled:true];
             [self.sliderXiaoJie setEnabled:false];
             [self.sliderSpeed setEnabled:true];
+            [self.btnRePlay setEnabled:false];
         }
 
         [self.btnXiaoJieTiaoZhuan setEnabled:true];
@@ -734,11 +740,13 @@
             [self.btnHandCtl setEnabled:true];
             [self.sliderXiaoJie setEnabled:true];
             [self.sliderSpeed setEnabled:true];
+            [self.btnRePlay setEnabled:true];
         }else if (_iPlayMode == 1){
             [self.sliderSpeed setEnabled:false];
             [self.btnHint setEnabled:true];
             [self.btnHandCtl setEnabled:true];
             [self.sliderXiaoJie setEnabled:false];
+            [self.btnRePlay setEnabled:true];
         }
     }
     
